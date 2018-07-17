efebd51d5e991f10bd268a6b59a9f98058be47cd
John Wang <john.wang@oriente.com>
2018-07-17 09:14:34 +0800

    0.3.3版本

--- lib/orientepodspecpush.rb
+++ lib/orientepodspecpush.rb
@@ -20,6 +20,23 @@ module Orientepodspecpush
       exit unless podspecFile != nil
     end
 
+    def package(opts)
+      puts "start package".green
+      cmd = []
+      cmd << ['bundle exec'] if shouldUseBundleExec
+      podPackage = "pod package #{specfile}"
+      cmd << [podPackage]
+      cmd << opts[:package] unless opts[:package] == nil
+      
+      system cmd.join(' ')
+
+      puts "finish package".green
+      # system "git add ."
+      # system "git commit -m 'upload framework'"
+      # system "git push origin master"
+
+    end
+    
     def ensureGitClean
       if `git status --porcelain`.length != 0
         puts "Repo is not clean; will not push new version".red
@@ -35,16 +52,26 @@ module Orientepodspecpush
     def makeLintCmd(opts)
       lintCmd = []
       lintCmd << ['bundle exec'] if shouldUseBundleExec
-      lintCmd << ["pod spec lint"]
-
+      lintCmd << "pod spec lint"
+ 
       # Build sources
-      sources = ["https://github.com/CocoaPods/Specs.git"]
-      sources << opts[:sources] unless opts[:sources] == nil
-      sourcesArg = "--sources=" + sources.join(",")
+      # sources = ["https://github.com/CocoaPods/Specs.git"]
+      # sources << opts[:sources] unless opts[:sources] == nil
+      # unless opts[:lint] == nil
+      #   sources << opts[:lint]
+      # end
+
+      
+      # sourcesArg = "--sources=" + sources.join(",")
+
+     
+     
 
       # Build lintCmd
       lintCmd << specfile
-      lintCmd << sourcesArg
+      # lintCmd << sourcesArg
+      lintCmd << opts[:sources] unless opts[:sources] == nil
+      lintCmd <<  ["#{opts[:lint]}"]
       lintCmd << ["--private"] unless opts[:private] == false
 
       # finalize
@@ -54,38 +81,65 @@ module Orientepodspecpush
     def makePushCmd(opts)
       cmd = []
       cmd << ['bundle exec'] if shouldUseBundleExec
-      cmd << ["pod repo push #{opts[:specRepo]} #{specfile} --allow-warnings"]
+      podRepoPush = "pod repo push #{opts[:specRepo]} #{specfile}"
+      cmd << [podRepoPush]
+      cmd << opts[:push] unless opts[:push] == nil      
 
       cmd.join(' ')
     end
 
     def updateVersion
-      puts "Please enter new version of the pod so we can tag, lint and push it! (e.g. 1.2.0)".blue
-      @podVersion = gets.chomp.downcase
+#      puts "Please enter new version of the pod so we can tag, lint and push it! (e.g. 1.2.0)".blue
+#      @podVersion = gets.chomp.downcase
+#
+#      puts "Please enter new a brief message to put in the git tag describing what's changed".blue
+#      @podVersionMessage = gets.chomp.downcase
 
-      puts "Please enter new a brief message to put in the git tag describing what's changed".blue
-      @podVersionMessage = gets.chomp.downcase
-
-      system "git tag -a #{@podVersion} -m '#{@podVersionMessage}'"
+#      system "git add ."
+#      system "git commit -m 'upload framework'"
+#
+# @podVersion = opts[:tag]
+# @podVersionMessage = opts[:tagCommitMsg]
+      if @podVersionMessage == nil
+        system "git tag -a #{@podVersion} -m 'add new tag'"
+      else
+        system "git tag -a #{@podVersion} -m '#{@podVersionMessage}'"
+      end
+      # system "git tag -a #{@podVersion} -m '#{@podVersionMessage}'"
       system "git push --tags"
 
       contents = File.read(specfile)
-      oldVersion = Regexp.new('[0-9.]{2,6}').match(Regexp.new('(s.version)\s*=.*\n').match(contents).to_s).to_s
+      oldVersion = Regexp.new('[0-9.]{2,8}').match(Regexp.new('(s.version)\s*=.*\n').match(contents).to_s).to_s
       File.write(specfile, contents.sub!(oldVersion, @podVersion))
+      
+#      cmd = []
+#      cmd << ['bundle exec'] if shouldUseBundleExec
+#      cmd << ["pod package #{specfile} --force"]
+#
+#      system cmd.join(' ')
+#      system "git add ."
+#      system "git commit -m 'upload framework'"
+#      system "git push origin master"
+
+      
+#      system "git tag -a #{@podVersion} -m '#{@podVersionMessage}'"
+#      system "git push --tags"
     end
 
     def rollbackTag
       puts "Rolling back git tags".green
+
+      system "git checkout ."
       system "git tag -d #{@podVersion}"
       system "git push -d origin #{@podVersion}"
       exit
     end
 
-    def executeLint(withWarnings)
+    def executeLint(withWarnings,opts)
       cmd = [@lintCmd]
-      cmd << "--allow-warnings" unless withWarnings == false
+      # cmd << "--allow-warnings" unless withWarnings == false
 
-      command = cmd.join(' ')
+      command = cmd.join('')
 
       puts "Executing: #{command}".green
       success = system command
@@ -93,14 +147,18 @@ module Orientepodspecpush
       if success == false && withWarnings == false
         # Try again?
         puts "Linting failed, try again by allowing warnings? [Y/n]".blue
-        gets.chomp.downcase == "y" ? executeLint(true) : rollbackTag
+        gets.chomp.downcase == "y" ? executeLint(true,opts) : rollbackTag
       elsif success == false && withWarnings == true
         puts "Even with warnings, something is wrong. Look for any errors".red
         rollbackTag
+      else
+        unless opts[:noPackage] == true
+          package(opts)
+        end
       end
     end
 
-    def executePush
+    def executePush(opts)
       puts "Executing: #{@pushCmd}".green
       success = system @pushCmd
 
@@ -111,6 +169,7 @@ module Orientepodspecpush
     end
 
     def commitThisRepo
+      system "git add ."
       puts "Congrats! The pod has been linted and successfully push to the spec repo! All that is left is to commit the podspec here!".green
 
       puts "Could not commit files, consider finishing by hand by performing a git commit and push. Your spec repo should be up to date".red unless system('git commit -am "[Versioning] Updating podspec"') == true
@@ -123,17 +182,33 @@ module Orientepodspecpush
         opt :workspace, "Path to cocoapod workspace", :type => :string
         opt :sources, "Comma delimited list of private repo sources to consider when linting private repo. Master is included by default so private repos can source master", :type => :string
         opt :private, "If set, assume the cocoapod is private and skip public checks"
+        opt :tag, "tag of the repo push to", :type => :string
+        opt :tagCommitMsg, "commit message of this tag", :type => :string
+        opt :lint, "pod spec lint 情况下所需要的参数，需要用引号括起来，例如'--allow-warnings --sources=some source address'",:type => :string
+        opt :package, "pod package  情况下所需要的参数，需要用引号括起来，例如'--force --no-mangle'",:type => :string
+        opt :push, "pod repo push 情况下所需要的参数，需要用引号括起来，例如'--verbose --use-libraries'",:type => :string
+        opt :noPackage, "If set, no need to package"
       end
+
+      puts "lint:#{opts[:lint]}"
+      puts "package:#{opts[:package]}"
+      puts "push:#{opts[:push]}"
+      puts "noPackage:#{opts[:noPackage]}"
+      puts "tag:#{opts[:tag]}"
       # Need these two
       Trollop::die :specRepo, "Spec Repo must be provided" if opts[:specRepo] == nil
       Trollop::die :workspace, "Workspace path must be provided" if opts[:workspace] == nil
+      Trollop::die :tag, "tag must be provided" if opts[:tag] == nil
 
       Dir.chdir(opts[:workspace]) do
         # Check
         ensureGitClean
         ensureSpecfile
+#        packageCode
 
         # User input
+        @podVersion = opts[:tag]
+        @podVersionMessage = opts[:tagCommitMsg]
         updateVersion
 
         # Cmds
@@ -141,8 +216,8 @@ module Orientepodspecpush
         @pushCmd = makePushCmd(opts)
 
         # execute
-        executeLint(false)
-        executePush
+        executeLint(true,opts)
+        executePush(opts)
 
         # Tidy up this repo!!
         commitThisRepo
